8b5b94693c508a37eaf4f2f29e4114d6f29b90ad
WeslleyIvis <weslley_reive@hotmail.com>
2023-08-12 20:15:39 -0300

    Inst Window

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        ResquetAPI requestAPI = new ResquetAPI();
 
-        requestAPI.request("https://pokeapi.co/api/v2/pokemon/ditto", "GET");
+        Window window = new Window();
+
+        window.start();
+
     }
 }
