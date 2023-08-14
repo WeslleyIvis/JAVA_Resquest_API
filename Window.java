bfff98285d49e9b5165ed6776bd313dead7a0c75
WeslleyIvis <weslley_reive@hotmail.com>
2023-08-13 22:50:23 -0300

    Window aplication, request comp

--- Window.java
+++ Window.java
@@ -1,39 +1,93 @@
 import javax.swing.*;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Window {
-    public void start() {
-        // Window
-        JFrame frame = new JFrame("Janela");
+public class Window extends ResquetAPI {
+    private JFrame frame = new JFrame("Window");
+    private JPanel mainPanel = new JPanel(new GridBagLayout());;
+
+    private GridBagConstraints gridBC = new GridBagConstraints();
+
+    private String[] methods = { "POST", "GET", "PUT", "DELETE" };
+
+    Window() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1300, 700);
+    }
 
-        frame.setSize(200, 200);
+    public JPanel setPanel(JPanel panel, int rows, int columns) {
+        return panel = new JPanel(new GridLayout(rows, columns));
+    }
 
-        JPanel mainPanel = new JPanel(new GridLayout(1, 2));
+    private void selectMethod() {
+        // Pré config do novo item grid adicionado
+        gridBC.gridx = 0;
+        gridBC.gridy = 0;
+        gridBC.fill = GridBagConstraints.HORIZONTAL;
+        gridBC.weightx = 1.0;
 
-        JTextField text = new JTextField(20);
-        mainPanel.add(text);
+        JPanel requestPanel = new JPanel();
+        requestPanel.setLayout(new BoxLayout(requestPanel, BoxLayout.X_AXIS));
 
-        JButton button = new JButton("Submit");
+        JComboBox<String> options = new JComboBox<>(methods);
+        requestPanel.add(options);
 
-        button.addActionListener(new ActionListener() {
+        JTextField urlAPI = new JTextField(null, "URL", 0);
+
+        requestPanel.add(urlAPI);
+
+        JButton requestButton = new JButton("Send");
+        requestPanel.add(requestButton);
+
+        requestButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                status();
+                String selected = (String) options.getSelectedItem();
+
+                request(urlAPI.getText(), selected);
             }
         });
 
-        mainPanel.add(button);
+        mainPanel.add(requestPanel, gridBC);
+    }
 
-        frame.add(mainPanel);
+    private void statusBar(int status) {
+        gridBC.gridx = 1;
+        gridBC.gridy = 0;
+        gridBC.fill = GridBagConstraints.NONE;
+
+        JPanel statusPanel = new JPanel();
+
+        JLabel statusLabel = new JLabel("Any");
+        statusPanel.add(statusLabel);
+
+        if (status == 400) {
+            statusLabel.setText(status + "Bad request");
+        }
+
+        mainPanel.add(statusLabel, gridBC);
+    }
+
+    private void consoleData() {
+        gridBC.gridx = 1;
+        gridBC.gridy = 1;
+
+        JLabel data = new JLabel();
+
+        mainPanel.add(data, gridBC);
+    }
+
+    public void start() {
+
+        gridBC.gridx = 0;
+
+        selectMethod();
+        statusBar(0);
+
+        frame.getContentPane().add(mainPanel);
         frame.setVisible(true);
     }
-
-    private void status() {
-        System.out.println("Status");
-    }
-
 }
